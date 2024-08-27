58b4b0d6d60fe9fa2d03675dd1a217c754430d04
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-08-27 11:23:51 +0700

    refactor: add vars in env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD4ZzCJ5JxlhhbO1H7kM5O9ZZkbrgtY0I",
-  authDomain: "managesite-478e5.firebaseapp.com",
-  projectId: "managesite-478e5",
-  storageBucket: "managesite-478e5.appspot.com",
-  messagingSenderId: "186916338749",
-  appId: "1:186916338749:web:f5c730a09fd9192051be54",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
